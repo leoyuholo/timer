73a0752212a859f53412838f79d6a37349daf6f8
leoyuholo <leoyuholo@gmail.com>
2014-08-04 22:09:38 +0800

    modified constructor

--- timer.js
+++ timer.js
@@ -1,27 +1,29 @@
 (function () {
 	var root = this;
 
-	var Timer = function (opt) {
-		if (!(this instanceof Timer)) {
-			return new Timer(opt);
-		} else {
-			this._ = {
-				now: Date.now(),
-				start: Date.now(),
-				end: 0,
-				frequency: 1000,
-				events: {},
-				timeout: ''
-			};
-			opt = opt instanceof Date ? {end: opt.getTime()} : 'number' === typeof opt ? {end: Date.now() + opt} : opt;
-			if ('object' === typeof opt) {
-				for (var prop in opt) {
-					this._[prop] = opt[prop];
-				}
-			}
-			this._.remain = this._.end - this._.now;
-			this._.elapsed = this._.now - this._.start;
-		}
+	var Timer = function (end, endCb, updateCb) {
+		var _ = {
+			now: Date.now(),
+			start: Date.now(),
+			end: 0,
+			remain: 0,
+			elapsed: 0,
+			frequency: 1000,
+			events: {
+				end: [],
+				update: []
+			},
+			timeout: ''
+		};
+
+		_.end = end ? end instanceof Date ? end.getTime() : 'number' === typeof end ? _.now + end : _.now + +end : 0;
+
+		if ('function' === typeof endCb)
+			_.events.end.push(endCb);
+		if ('function' === typeof updateCb)
+			_.events.update.push(updateCb);
+
+		this._ = _;
 	};
 
 	// TODO: export to module.exports for node.js compatibility
